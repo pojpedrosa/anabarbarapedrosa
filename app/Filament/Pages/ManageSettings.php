30f7114510bae747c7c8774a1871053cc807b92c
Pedro Pedrosa <pojpedrosa@Pedros-MacBook-Pro-2.local>
2026-04-29 16:47:57 +0100

    Fix ManageSettings form() to use Filament 5 Schema type

--- app/Filament/Pages/ManageSettings.php
+++ app/Filament/Pages/ManageSettings.php
@@ -9,9 +9,9 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 
 class ManageSettings extends Page implements HasForms
@@ -34,7 +34,7 @@ class ManageSettings extends Page implements HasForms
         );
     }
 
-    public function form(Form $form): Form
+    public function form(Schema $form): Schema
     {
         return $form->schema([
             Textarea::make('bio')->label('Bio / Tagline')->rows(3),
